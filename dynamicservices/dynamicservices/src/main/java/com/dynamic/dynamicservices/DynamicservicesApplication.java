a8362b6e6bca0a02cbc368c9810d59cb88dd7a1a
Varsha692 <118357504+Varsha692@users.noreply.github.com>
2022-11-18 13:22:29 +0530

    Added Services again

---
+++ dynamicservices/dynamicservices/src/main/java/com/dynamic/dynamicservices/DynamicservicesApplication.java
@@ -0,0 +1,13 @@
+package com.dynamic.dynamicservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicservicesApplication.class, args);
+	}
+
+}
---
+++ dynamicservices/dynamicservices/src/test/java/com/dynamic/dynamicservices/DynamicservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic.dynamicservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
